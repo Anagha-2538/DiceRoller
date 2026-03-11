3198e1a2e251320a9c5a5f5c41c73d6418a11fe7
Anagha <anagha6393@gmail.com>
2026-03-11 17:00:13 +0530

    Implement dice roll logic using Compose state

--- app/src/main/java/com/example/dicerollerapp/MainActivity.kt
+++ app/src/main/java/com/example/dicerollerapp/MainActivity.kt
@@ -15,6 +15,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -45,6 +49,15 @@ fun DiceRollerApp() {
 fun DiceWithButtonAndImage(
     modifier: Modifier = Modifier
 ) {
+    var result by remember { mutableStateOf(1) }
+    val imageResource = when (result){
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -53,13 +66,13 @@ fun DiceWithButtonAndImage(
     ) {
 
         Image(
-            painter = painterResource(R.drawable.dice_1),
-            contentDescription = "Dice showing 1"
+            painter = painterResource(imageResource),
+            contentDescription = result.toString()
         )
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        Button(onClick = { }) {
+        Button(onClick = { result = (1..6).random() }) {
             Text(stringResource(R.string.roll))
         }
     }
