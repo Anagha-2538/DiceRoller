534091593bd3d70f6a0429c5c529ed7429c751e0
Anagha-2538 <153118813+Anagha-2538@users.noreply.github.com>
2026-08-14 10:44:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dicerollerapp/MainActivity.kt
@@ -0,0 +1,101 @@
+package com.example.dicerollerapp
+
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.dicerollerapp.ui.theme.DiceRollerAppTheme
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.ui.draw.rotate
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DiceRollerAppTheme {
+                DiceRollerApp()
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun DiceRollerApp() {
+    DiceWithButtonAndImage()
+}
+
+@Composable
+fun DiceWithButtonAndImage(
+    modifier: Modifier = Modifier
+) {
+    var result by remember { mutableStateOf(1) }
+    var rotation by remember { mutableStateOf(0f) }
+
+    val animatedRotation by animateFloatAsState(
+        targetValue = rotation,
+        label = ""
+    )
+
+    val imageResource = when (result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Image(
+            painter = painterResource(imageResource),
+            contentDescription = result.toString(),
+            modifier = Modifier
+                .height(200.dp)
+                .rotate(animatedRotation)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {
+                result = (1..6).random()
+                rotation += 360f
+            },
+            modifier = Modifier.padding(top = 24.dp)
+        ) {
+            Text(text = stringResource(R.string.roll))
+        }
+    }
+}
+
+
